f5bad2c180adb6d2b9b4793b62a6c9dd26cf4a26
Bkhuu149 <bhkhuu@uci.edu>
2023-01-19 12:51:54 -0800

    Added function declaractions for player inputs and implemented sprinting

--- Source/UnnamedRPG/MyRPGCharacter.cpp
+++ Source/UnnamedRPG/MyRPGCharacter.cpp
@@ -31,15 +31,121 @@ void AMyRPGCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComp
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 	PlayerInputComponent->BindAxis(TEXT("ForwardBack"), this, &AMyRPGCharacter::MoveForwardBack);
 	PlayerInputComponent->BindAxis(TEXT("RightLeft"), this, &AMyRPGCharacter::MoveRightLeft);
+
+	PlayerInputComponent->BindAction(TEXT("Jump"), EInputEvent::IE_Pressed, this, &AMyRPGCharacter::OnJumpedPressed);
+
+	PlayerInputComponent->BindAction(TEXT("Target"), EInputEvent::IE_Pressed, this, &AMyRPGCharacter::OnTargetPressed);
+
+	PlayerInputComponent->BindAction(TEXT("Block"), EInputEvent::IE_Pressed, this, &AMyRPGCharacter::OnBlockPressed);
+	PlayerInputComponent->BindAction(TEXT("Block"), EInputEvent::IE_Released, this, &AMyRPGCharacter::OnBlockReleased);
+
+	PlayerInputComponent->BindAction(TEXT("Heal"), EInputEvent::IE_Pressed, this, &AMyRPGCharacter::OnHealPressed);
+
+	PlayerInputComponent->BindAction(TEXT("Dodge"), EInputEvent::IE_Pressed, this, &AMyRPGCharacter::OnDodgePressed);
+
+	PlayerInputComponent->BindAction(TEXT("Sprint"), EInputEvent::IE_Pressed, this, &AMyRPGCharacter::OnSprintPressed);
+	PlayerInputComponent->BindAction(TEXT("Sprint"), EInputEvent::IE_Released, this, &AMyRPGCharacter::OnSprintReleased);
+
+	PlayerInputComponent->BindAction(TEXT("Attack 1"), EInputEvent::IE_Pressed, this, &AMyRPGCharacter::OnAttackPressed);
+
+	PlayerInputComponent->BindAction(TEXT("Finsher"), EInputEvent::IE_Pressed, this, &AMyRPGCharacter::OnFinisherPressed);
+
+	PlayerInputComponent->BindAction(TEXT("Interact"), EInputEvent::IE_Pressed, this, &AMyRPGCharacter::OnInteractPressed);
+
+	PlayerInputComponent->BindAction(TEXT("Menu"), EInputEvent::IE_Pressed, this, &AMyRPGCharacter::OnMenuPressed);
+
+	PlayerInputComponent->BindAction(TEXT("Inventory"), EInputEvent::IE_Pressed, this, &AMyRPGCharacter::OnInventoryPressed);
 }
 
 void AMyRPGCharacter::MoveForwardBack(float value) {
 	FVector Direction = FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::X);
-	AddMovementInput(Direction, value);
+	AddMovementInput(Direction, value * SprintMultiplier);
 }
 
 void AMyRPGCharacter::MoveRightLeft(float value)
 {
 	FVector Direction = FRotationMatrix(Controller->GetControlRotation()).GetScaledAxis(EAxis::Y);
-	AddMovementInput(Direction, value);
+	AddMovementInput(Direction, value * SprintMultiplier);
+}
+
+void AMyRPGCharacter::OnBlockPressed() {
+	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Blocked Pressed"));
+	//Start a timer to see how long this button was pressed to distinguish between blocking and parrying
+	IsBlocking = true;
+	PressedTime = FDateTime::Now().GetTicks();
+
+
+}
+
+void AMyRPGCharacter::OnBlockReleased() {
+
+	//Condiiton should check if timer is past a certain time limit. If true, then perform parry.  False would release block
+	int64 CurrentTime = FDateTime::Now().GetTicks();
+
+	if ((CurrentTime - PressedTime)/1000 <= PARRY_THRESH) {
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Parry"));
+		IsBlocking = false;
+		return;
+	}
+
+	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Block"));
+	IsBlocking = false;
+	return;
+
+}
+
+void AMyRPGCharacter::OnHealPressed() {
+	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Heal"));
+}
+
+void AMyRPGCharacter::OnDodgePressed() {
+	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Dodge"));
+}
+
+void AMyRPGCharacter::OnJumpedPressed() {
+	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Jump"));
+}
+
+void AMyRPGCharacter::ReleaseJump() {
+	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Jump Released"));
+}
+
+void AMyRPGCharacter::OnTargetPressed() {
+	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Targeting"));
+}
+
+void AMyRPGCharacter::OnAttackPressed() {
+	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Attack"));
+}
+
+void AMyRPGCharacter::ResetAttack() {
+	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Attack Reset"));
+}
+
+void AMyRPGCharacter::OnFinisherPressed() {
+	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Finisher Pressed"));
+}
+
+void AMyRPGCharacter::OnInteractPressed() {
+	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Interact"));
+}
+
+void AMyRPGCharacter::OnMenuPressed() {
+	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Open Menu"));
+}
+
+void AMyRPGCharacter::OnInventoryPressed() {
+	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Open Inventory"));
+}
+
+void AMyRPGCharacter::OnSprintPressed() {
+	//GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Sprinting"));
+	SprintMultiplier = 3.f;
+	IsSprinting = true;
+}
+
+void AMyRPGCharacter::OnSprintReleased() {
+	//GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Stop Sprinting"));
+	SprintMultiplier = .5f;
+	IsSprinting = false;
 }
--- Source/UnnamedRPG/MyRPGCharacter.h
+++ Source/UnnamedRPG/MyRPGCharacter.h
@@ -19,6 +19,61 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+private:
+
+	//Handle Jump Input
+	void OnJumpedPressed();
+	bool IsJumping = false;
+	FTimerHandle* JumpTimer;
+	void ReleaseJump();
+
+	//Handle Target Pressed
+	void OnTargetPressed();
+	bool Targeted;
+	AActor* Target;
+
+	//Handle Block Inputs
+	void OnBlockPressed();
+	void OnBlockReleased();
+	bool IsBlocking = false;
+	int64 PressedTime = 0;
+	const int PARRY_THRESH = 1000;
+
+	//Handle Healing Inputs
+	void OnHealPressed();
+	float Mana = 1000;
+
+	//Handle Dodge Input
+	void OnDodgePressed();
+	bool IsDodging = false;
+
+	//Handle Sprint Input
+	void OnSprintPressed();
+	void OnSprintReleased();
+	bool IsSprinting = false;
+	float SprintMultiplier = .5f;
+
+	//Handle Attack Pressed
+	void OnAttackPressed();
+	int AttackCount = 0;
+	void ResetAttack();
+	FTimerHandle* AttackTimer; 
+
+	//Handle Finisher Pressed
+	void OnFinisherPressed();
+
+	//Handle Interact Pressed
+	void OnInteractPressed();
+	bool IsInteracting;
+	
+	//Handle Menu Pressed
+	void OnMenuPressed();
+	bool InMenu;
+
+	//Handle Inventory Pressed
+	void OnInventoryPressed();
+	bool InInventory;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
@@ -29,5 +84,23 @@ public:
 	void MoveForwardBack(float value);
 
 	void MoveRightLeft(float value);
+
+	bool GetIsBlocking() { return IsBlocking; }
+
+	bool GetIsDodging() { return IsDodging; }
+
+	bool GetIsSprinting() { return IsSprinting; }
+
+	int GetAttackCount() { return AttackCount; }
+
+	bool GetIsInteracting() { return IsInteracting; }
+
+	bool GetIsInMenu() { return InMenu; }
+
+	bool GetIsInInventory() { return InInventory; }
+
+	bool GetIsJumping() { return IsJumping; }
+
+	bool GetIsTargeted() { return Targeted; }
 };
 
