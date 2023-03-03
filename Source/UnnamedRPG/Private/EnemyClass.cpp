aba130d860e11a39c95d754bb36bfa4a9f1f8779
Bkhuu149 <bhkhuu@uci.edu>
2023-03-03 15:12:07 -0800

    Got enemy ai behavior working, need to convert to c++

--- Source/UnnamedRPG/Private/EnemyClass.cpp
+++ Source/UnnamedRPG/Private/EnemyClass.cpp
@@ -2,4 +2,63 @@
 
 
 #include "EnemyClass.h"
+#include "Components/SphereComponent.h"
+
+// Called to bind functionality to input
+AEnemyClass::AEnemyClass()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	TriggerSphere = CreateDefaultSubobject<USphereComponent>(TEXT("Trigger Sphere"));
+	TriggerSphere->InitSphereRadius(500.f);
+	TriggerSphere->SetCollisionProfileName(TEXT("Trigger"));
+	TriggerSphere->SetupAttachment(RootComponent);
+
+	TriggerSphere->OnComponentBeginOverlap.AddDynamic(this, &AEnemyClass::OnOverlapBegin);
+
+
+}
+
+void AEnemyClass::BeginPlay()
+{
+	Super::BeginPlay();
+	SpawnLocation = GetActorLocation();
+	MyController = static_cast<AAIController*>(GetController());
+
+
+}
+
+void AEnemyClass::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	
+	if (Targeted) {
+
+	}
+	else {
+		if (!DelayTimer.IsValid()){
+			GetWorld()->GetTimerManager().SetTimer(DelayTimer, this, &AEnemyClass::Walk, 10.f, false);
+		}
+	}
+
+
+}
+
+void AEnemyClass::Walk() {
+	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Walk"));
+	
+	DelayTimer.Invalidate();
+}
+
+
+void AEnemyClass::OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor && (OtherActor != this) && OtherComp)
+	{
+		Targeted = true;
+		Target = OtherActor;
+		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Overlap Begin"));
+	}
+}
 
--- Source/UnnamedRPG/Public/EnemyClass.h
+++ Source/UnnamedRPG/Public/EnemyClass.h
@@ -4,14 +4,45 @@
 
 #include "CoreMinimal.h"
 #include "RPGBaseClass.h"
+#include "AIController.h"
 #include "EnemyClass.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class UNNAMEDRPG_API AEnemyClass : public ARPGBaseClass
 {
 	GENERATED_BODY()
-	
+
+	UPROPERTY(VisibleAnywhere, Category = "Trigger Capsule")
+	class USphereComponent* TriggerSphere;
+
+public: 
+	// Sets default values for this character's properties
+	AEnemyClass();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Abilities")
+	bool IsMoving;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Abilities")
+	bool Targeted;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Abilities")
+	AActor* Target;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Abilities")
+	FVector SpawnLocation;
+private:
+	FTimerHandle DelayTimer;
+
+	AAIController* MyController;
+
+	void Walk();
+
+public: 
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+	void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+
 };
