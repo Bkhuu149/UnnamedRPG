896dcc2ce46dce2c0abfaf52b72679bc929ac153
Bkhuu149 <bhkhuu@uci.edu>
2023-03-29 15:02:07 -0700

    Started converting PushableActor to cpp

--- Source/UnnamedRPG/Private/PushableActor.cpp
+++ Source/UnnamedRPG/Private/PushableActor.cpp
@@ -25,3 +25,44 @@ void APushableActor::Tick(float DeltaTime)
 
 }
 
+int APushableActor::FindClosestPushTransformIndex1(FVector2D CharacterLocation, float PushRange){
+	int CurrentTransformIndex = 0;
+	int ClosestTransformIndex = -1;
+	float CurrentDistanceSq = 0.f;
+	float ClosestDistanceSq = 0.f;
+
+	for (int i = 0; i < PushTransformsTemp.Max(); i++)
+	{
+		FTransform Transform = PushTransformsTemp[i];
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("CurrentPushTransform %i"), i));
+		CurrentTransformIndex = i;
+		FVector2D MyLocation = FVector2D(UKismetMathLibrary::TransformLocation(GetActorTransform(), Transform.GetLocation()));
+		CurrentDistanceSq = UKismetMathLibrary::DistanceSquared2D(MyLocation, CharacterLocation);
+		if (CurrentDistanceSq < pow(PushRange, 2) && ((ClosestDistanceSq < CurrentDistanceSq) || ClosestTransformIndex < 0)) {
+			ClosestTransformIndex = CurrentTransformIndex;
+			ClosestDistanceSq = CurrentDistanceSq;
+		}
+	}
+
+	return ClosestTransformIndex;
+}
+
+void APushableActor::HandleInteraction1(ACharacter* Character) {
+
+	if (!Character) {
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, (TEXT("Character not valid")));
+		return;
+	}
+
+	UMyPushComponent* PushComp = Cast<UMyPushComponent>(Character->GetComponentByClass(UMyPushComponent::StaticClass()));
+	if (!PushComp) {
+		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, (TEXT("Character does not have push component")));
+		return;
+	}
+
+	FindClosestPushTransformIndex1(FVector2D(Character->GetActorLocation()), PushComp->PushRange);	
+
+	return;
+}
+
+
--- Source/UnnamedRPG/Public/PushableActor.h
+++ Source/UnnamedRPG/Public/PushableActor.h
@@ -3,7 +3,10 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameFramework/Character.h"
 #include "GameFramework/Actor.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "MyPushComponent.h"
 #include "PushableActor.generated.h"
 
 UCLASS()
@@ -18,9 +21,20 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TArray<FTransform> PushTransformsTemp;
 
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable)
+	int FindClosestPushTransformIndex1(FVector2D CharacterLocation, float PushRange);
+
+	UFUNCTION(BlueprintCallable)
+	void HandleInteraction1(ACharacter* Character);
+
+
 
 };
